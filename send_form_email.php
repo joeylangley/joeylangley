4dc650b1575da4f6cff40aa94bff4d6b22a98add
Joey Langley <joemagnum1@hotmail.com>
2016-05-24 15:06:42 -0400

    yet another form

--- send_form_email.php
+++ send_form_email.php
@@ -34,13 +34,15 @@ if(isset($_POST['email'])) {
  
     // validation expected data exists
  
-    if(!isset($_POST['name']) ||
+    if(!isset($_POST['first_name']) ||
+ 
+        !isset($_POST['last_name']) ||
  
         !isset($_POST['email']) ||
  
         !isset($_POST['telephone']) ||
  
-        !isset($_POST['message'])) {
+        !isset($_POST['comments'])) {
  
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
  
@@ -48,13 +50,15 @@ if(isset($_POST['email'])) {
  
      
  
-    $name = $_POST['name']; // required
+    $first_name = $_POST['first_name']; // required
+ 
+    $last_name = $_POST['last_name']; // required
  
     $email_from = $_POST['email']; // required
  
     $telephone = $_POST['telephone']; // not required
  
-    $message = $_POST['message']; // required
+    $comments = $_POST['comments']; // required
  
      
  
@@ -70,16 +74,21 @@ if(isset($_POST['email'])) {
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
-  if(!preg_match($string_exp,$name)) {
+  if(!preg_match($string_exp,$first_name)) {
  
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
  
   }
  
-
-  if(strlen($message) < 2) {
+  if(!preg_match($string_exp,$last_name)) {
  
-    $error_message .= 'The message you entered does not appear to be valid.<br />';
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+ 
+  }
+ 
+  if(strlen($comments) < 2) {
+ 
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
  
   }
  
@@ -103,13 +112,15 @@ if(isset($_POST['email'])) {
  
      
  
-    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
  
-    $email_message .= "Email: ".clean_string($email)."\n";
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
  
     $email_message .= "Telephone: ".clean_string($telephone)."\n";
  
-    $email_message .= "Message: ".clean_string($message)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
  
      
  
